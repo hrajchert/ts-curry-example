ce4a46dc1d1bd4e86dc11263b300f064955d0724
Hernan Rajchert <hrajchert@gmail.com>
2018-07-31 17:54:15 -0300

    chore(cleanup): remove old files

--- src/foo/foo.test.ts
+++
@@ -1,9 +0,0 @@
-import {assert} from 'chai';
-import 'mocha';
-import {add} from './foo';
-
-describe('add:', () => {
-    it('add(1 + 1) = 2', () => {
-        assert.deepEqual(add(1, 1), 2);
-    });
-});
